f7837d5cc3da04b2909674fd07df369f489c8b86
alina <alina.meller.98@mail.ru>
2025-06-17 16:45:16 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+public class Main {
+    public static void main(String[] args) {
+//        Печатаем строку
+        /*
+         * Печатаем строку
+         * Печатаем строку
+         * Печатаем строку
+         */
+//        System.out.println("Hello world!\nHello world!");
+//        System.out.println();
+////       System.out.print(); // ошибка
+//
+//        System.out.println("           Hello");
+
+//      Конкатенация
+//        System.out.println("3" + "3");
+//        System.out.println("3" + "3" + "56");
+//        System.out.println(3 + 3);
+//        System.out.println("Результат: " + 10);
+        System.out.println("Результат: " + (10 + 3));
+    }
+}
