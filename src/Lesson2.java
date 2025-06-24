24b8a80aab17f7ea47078ed30562d52ccad1d6d2
alina <alina.meller.98@mail.ru>
2025-06-24 16:52:34 +0300

    lesson2

--- src/Lesson2.java
+++ src/Lesson2.java
@@ -1,2 +1,42 @@
 public class Lesson2 {
+    public static void main(String[] args) {
+        //        System.out.println("Результат: " + (10 + 3));
+//        System.out.println(10 + 3);
+//        System.out.println(10 - 3);
+//        System.out.println(10 * 3);
+//        System.out.println(10 / 3);
+//        System.out.println(10.0 / 3.0);
+        // Остаток от деления
+//        System.out.println(10 % 3);
+
+//        кол-во конфет 21
+//        кол-во гостей 5
+//        По сколько целых конфет достанется кадодому гостю
+
+//        System.out.println("Результат: " + 21 % 5 );
+
+//        System.out.println("kjdghjkhjkhfjkhdsfkjhsfdkjhsdfjhkdfjshkjdhsfjkd");
+//
+//
+//        System.out.println("kjdghjkhjkhfjkhdsfkjhsfdkjhsdfjhkdfjshkjdhsfjkd");
+//
+//        System.out.println("kjdghjkhjkhfjkhdsfkjhsfdkjhsdfjhkdfjshkjdhsfjkd");
+
+        byte age = 10;
+
+        byte amount;
+        amount = 20;
+
+        amount = age;
+        System.out.println(amount);
+//        System.out.println(amount);
+//
+//        amount = 89;
+//
+//        System.out.println(amount);
+
+
+        System.out.println(age);
+//        name = "dsjfjfksjfkdjsfkjsfksfj";
+    }
 }
