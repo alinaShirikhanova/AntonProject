37c096330211a3de61ae6c7f4d0b749211e4c369
alina <alina.meller.98@mail.ru>
2025-07-08 18:27:41 +0300

    lesson4

---
+++ src/Lesson4.java
@@ -0,0 +1,57 @@
+import java.util.Scanner;
+
+public class Lesson4 {
+    public static void main(String[] args) {
+//        System.out.println(90.9);
+//        float f = 12.3456789123456789f;
+//        System.out.println(f); // 6-7 значащих цифр
+//
+//        double d = 123456789123456789123456789123456789123456789123456789123456789123456789.0;
+//        System.out.println(d); // 16-17 значащих цифр
+//        Character - символ
+//        char ch1 = 'A';
+//        char ch2 = 'B';
+//        int num = 'A';
+//        char sym = 70;
+//        System.out.println(ch1 + "" + ch2);
+//        System.out.println(num);
+//        System.out.println(sym);
+
+
+        // Оператор сравнения
+//        >
+//        <
+//        >=
+//        <=
+//        == (равно)
+//        != (не равно)
+//        ! - ЛОГИЧЕСКОЕ ОТРИЦАНИЕ (НЕ)
+//        int a = 9;
+//        int b = 9;
+//        System.out.println(10 == 10);
+
+//        int x = 13;
+//        boolean res = x > 10;
+//        System.out.println(res);
+
+//        &&
+
+
+//        int x = 13;
+//        boolean res = 5 < x && x < 10;
+
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int cnt = 1;
+
+        int[] array = new int[n];
+        for (int i = n; i > 0; i--) {
+            array[i - 1] = cnt;
+            cnt += 1;
+        }
+
+        for (int i = 0; i < n; i++) {
+            System.out.print(array[i] + " ");
+        }
+    }
+}
