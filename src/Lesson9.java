e9de3ab0c6693bcf8d88d24c888d381d0e5b5d95
Alina <alina.meller.98@mail.ru>
2025-08-07 17:59:28 +0300

    lesson9

---
+++ src/Lesson9.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class Lesson9 {
+    public static void main(String[] args) {
+//        int a = 9;
+//        if (a > 10){
+//            System.out.println("yes");
+//        } else {
+//            System.out.println("no");
+//        }
+//
+//        System.out.println(a > 10 ? "yes" : "no");
+//        byte
+//        short
+//        int
+//        long
+//        float
+//        double
+//        char - символы
+//        boolean
+
+//        boolean res = 4 > 6;
+
+//        String - нить
+
+        Scanner sc = new Scanner(System.in);
+//        int i = 0;
+//        String line = "Hello";
+////        String s = sc.nextLine();
+//        System.out.println(line.length());
+
+        // Длина строки - кол-во символов в строке
+//        Character - символ
+        String line = "tables";
+        System.out.println(line.charAt(0));
+    }
+}
