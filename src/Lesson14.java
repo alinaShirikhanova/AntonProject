e267672c1b47c6c46ce43f7b47eeb1af602f6cc6
Alina <alina.meller.98@mail.ru>
2025-08-25 16:36:47 +0300

    lesson13

---
+++ src/Lesson14.java
@@ -0,0 +1,32 @@
+public class Lesson14 {
+    public static void main(String[] args) {
+//        Есть ли среди двузначных чисел число, которое делится и на 3, и на 7
+        boolean hasNumber = false;
+
+//        for (int i = 10; i <= 99; i++) {
+//            if (i % 3 == 0 && i % 100 == 0) {
+//                hasNumber = true;
+//                break;
+//            }
+//        }
+//
+//        if (hasNumber){
+//            System.out.println("Есть");
+//        } else {
+//            System.out.println("Нет");
+//        }
+
+
+//        Есть ли среди трехзначных чисел число, которое делится и на 10, и на 3
+        // Есть
+        // Нет
+
+
+        int sum = 0;
+        for (int i = 1; i <= 10; i++) {
+            sum += i;
+        }
+
+
+    }
+}
