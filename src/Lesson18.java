cdae7a9885643d0ff2a1fd10f9fc13b007d074b7
Alina <alina.meller.98@mail.ru>
2025-09-15 22:04:26 +0300

    lesson17

---
+++ src/Lesson18.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Lesson18 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+//        int n = sc.nextInt();
+//        int[] array = new int[n];
+//
+//        for (int i = 0; i < array.length; i++) {
+//            array[i] = sc.nextInt();
+//        }
+
+        // Найти максимальный элемент
+//        [4, 6, 8, 10, 1]
+//        int max = array[0];
+//        for (int i = 0; i < array.length; i++) {
+//            if (array[i] > max) {
+//                max = array[i];
+//            }
+//        }
+//        System.out.println(max);
+
+
+        int[] arr = {1, 4, 7, 2, 10};
+
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] > arr[i - 1]) {
+                System.out.println("Нашел");
+            }
+        }
+
+    }
+}
