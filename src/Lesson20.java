aafdb6f2c40b9a70e67a0fcafa1ff4cc1b163bf1
Alina <alina.meller.98@mail.ru>
2025-09-29 22:49:31 +0300

    lesson20

--- src/Lesson20.java
+++ src/Lesson20.java
@@ -23,22 +23,23 @@ public class Lesson20 {
 //        }
 //
 
-        int[][] array = {
-                {1, 2, 3, 4},
-                {5, 6, 7, 8},
-                {9, 10, 11, 12}
-        };
-
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 4; j++) {
-                System.out.println(array[i][j]);
-            }
-
-        }
+//        int[][] array = {
+//                {1, 2, 3, 4},
+//                {5, 6, 7, 8},
+//                {9, 10, 11, 12}
+//        };
+//
+//        for (int i = 0; i < 3; i++) {
+//            for (int j = 0; j < 4; j++) {
+//                System.out.println(array[i][j]);
+//            }
+//        }
 
 //        System.out.println(array[0]);
 //        System.out.println(array[1]);
 //        System.out.println(array[2]);
+
+
 
 
     }
---
+++ src/Lesson21.java
@@ -0,0 +1,49 @@
+import java.util.Scanner;
+
+public class Lesson21 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+//        int n = sc.nextInt(); // кол-во ячеек во внешнем массиве
+//        int m = sc.nextInt(); // кол-во ячеек в каждом из вложенных массивов
+//
+//        int[][] arr = new int[n][m];
+//
+//        for (int i = 0; i < n; i++) {
+//            for (int j = 0; j < m; j++) {
+//                arr[i][j] = sc.nextInt();
+//            }
+//        }
+
+
+//       int sum = 0;
+//        for (int i = 0; i <n; i++) {
+//            for (int j = 0; j <m; j++) {
+//                sum += arr[i][j];
+//            }
+//        }
+
+
+
+
+
+
+
+//        for (int i = 0; i < n; i++) {
+//            for (int j = 0; j < m; j++) {
+//                System.out.print(arr[i][j] + " ");
+//            }
+//            System.out.println();
+//        }
+
+
+
+
+
+
+
+//        System.out.println(arr);
+
+
+    }
+}
